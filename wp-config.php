b40ce537fc68986e689238967284ed6ef470cae7
Gavin Peng <gavin4@hawaii.edu>
2023-04-18 23:17:38 -1000

    add ui-testing

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wpdatabase' );
+define( 'DB_NAME', 'ics-496-capstone' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
